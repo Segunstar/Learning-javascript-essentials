37cdff60fa5915e85b19934d18890b99871660c4
Star boy <ekundayoisegun@gmail.com>
2022-10-29 20:29:04 +0100

    how to fully use gulp and babel

--- gulp.js
+++
@@ -1,5 +0,0 @@
-const gulp = require('gulp');
-// gulp dependecies go here
-gulp.task('default', function () {
- //gulp tasks go here
-});
---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+gulp.task("default", async function () {
+  // Node source
+
+  gulp.src("es6/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
+  // browser source
+  gulp.src("public/es6/**/*.js").pipe(babel()).pipe(gulp.dest("public/dist"));
+});
