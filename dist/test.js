e2fe5f325f9b19bad04c2ea90fab20eb1d7aa3e8
Star boy <ekundayoisegun@gmail.com>
2022-10-31 11:38:18 +0000

    control flow learnt

--- dist/test.js
+++ dist/test.js
@@ -1,26 +1,65 @@
 "use strict";
 
-// es6 feature: block-scoped "let" declaration
-var sentences = [{
-  subject: 'JavaScript',
-  verb: 'is',
-  object: 'great'
-}, {
-  subject: 'Elephants',
-  verb: 'are',
-  object: 'large'
-}];
-// es6 feature: object destructuring
-function say(_ref) {
-  var subject = _ref.subject,
-    verb = _ref.verb,
-    object = _ref.object;
-  // es6 feature: template strings
-  // note that quotes below are backticks (`), not single quotes (')
-  console.log("".concat(subject, " ").concat(verb, " ").concat(object));
+// returns a random integer in the range [m, n] (inclusive)
+function rand(m, n) {
+  return m + Math.floor((n - m + 1) * Math.random());
 }
-// es6 feature: for..of
-for (var _i = 0, _sentences = sentences; _i < _sentences.length; _i++) {
-  var s = _sentences[_i];
-  say(s);
+
+// randomly returns a string representing one of the six 
+// Crown and Anchor faces
+function randFace() {
+  return ["crown", "anchor", "heart", "spade", "club", "diamond"][rand(0, 5)];
 }
+var funds = 50;
+// starting conditions
+var round = 0;
+var _loop = function _loop() {
+  round++;
+  console.log("round ".concat(round, ":"));
+  console.log("\tstarting funds: ".concat(funds, "p"));
+  // place bets  
+  var bets = {
+    crown: 0,
+    anchor: 0,
+    heart: 0,
+    spade: 0,
+    club: 0,
+    diamond: 0
+  };
+  var totalBet = rand(1, funds);
+  if (totalBet === 7) {
+    totalBet = funds;
+    bets.heart = totalBet;
+  } else {
+    // distribute total bet    
+    var remaining = totalBet;
+    do {
+      var bet = rand(1, remaining);
+      var face = randFace();
+      bets[face] = bets[face] + bet;
+      remaining = remaining - bet;
+    } while (remaining > 0);
+  }
+  funds = funds - totalBet;
+  console.log('\tbets: ' + Object.keys(bets).map(function (face) {
+    return "".concat(face, ": ").concat(bets[face], " pence");
+  }).join(', ') + " (total: ".concat(totalBet, " pence)"));
+  // roll dice  
+  var hand = [];
+  for (var roll = 0; roll < 3; roll++) {
+    hand.push(randFace());
+  }
+  console.log("\thand: ".concat(hand.join(', ')));
+  // collect winnings   
+  var winnings = 0;
+  for (var die = 0; die < hand.length; die++) {
+    var _face = hand[die];
+    if (bets[_face] > 0) winnings = winnings + bets[_face];
+  }
+  funds = funds + winnings;
+  console.log("\twinnings: ".concat(winnings));
+};
+while (funds > 1 && funds < 100) {
+  _loop();
+}
+console.log("\tending funs: ".concat(funds));
