e3f5686a1e08a0be5158143ad6b6a37d0c16c0ab
Star boy <ekundayoisegun@gmail.com>
2022-11-01 17:39:50 +0100

    many things done to it

--- es6/test.js
+++ es6/test.js
@@ -1,61 +1,38 @@
-// returns a random integer in the range [m, n] (inclusive)
-function rand(m, n) {
-  return m + Math.floor((n - m + 1) * Math.random());
-}
-
-// randomly returns a string representing one of the six
-// Crown and Anchor faces
-function randFace() {
-  return ["crown", "anchor", "heart", "spade", "club", "diamond"][rand(0, 5)];
-}
-let funds = 50;
-// starting conditions
-let round = 0;
-while (funds > 1 && funds < 100) {
-  round++;
-  console.log(`round ${round}:`);
-  console.log(`\tstarting funds: ${funds}p`);
-  // place bets
-  let bets = { crown: 0, anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0 };
-  let totalBet = rand(1, funds);
-  if (totalBet === 7) {
-    totalBet = funds;
-    bets.heart = totalBet;
-  } else {
-    // distribute total bet
-    let remaining = totalBet;
-    // console.log(`-1-remaining is ${remaining}----`);
-    do {
-      let bet = rand(1, remaining);
-      let face = randFace();
-      // console.log(`-1-bet-face ${bets[face]}`);
-      bets[face] = bets[face] + bet;
-      // console.log(`-2-bet-face ${bets[face]}`);
-      remaining = remaining - bet;
-      // console.log(`-2-remaining is ${remaining}----`)
-    } while (remaining > 0);
-  }
-  funds = funds - totalBet;
-  console.log(
-    "\tbets: " +
-      Object.keys(bets)
-        .map((face) => `${face}: ${bets[face]} pence`)
-        .join(", ") +
-      ` (total: ${totalBet} pence)`
-  );
-  // roll dice
-  const hand = [];
-  for (let roll = 0; roll < 3; roll++) {
-    hand.push(randFace());
-  }
-  console.log(`\thand: ${hand.join(", ")}`);
-  // collect winnings
-  let winnings = 0;
-  for (let die = 0; die < hand.length; die++) {
-    let face = hand[die];
-    if (bets[face] > 0) winnings = winnings + bets[face];
-  }
-  funds = funds + winnings;
-  console.log(`\twinnings: ${winnings}`);
-}
-console.log(`\tending funs: ${funds}`);
+// let container = [
+//   {
+//     name: "Gbemiro",
+//     class: "S.S.S 2",
+//     age: 16
+//   }
+//   , {
+//     name: "Precious",
+//     class: "S.S.S 3",
+//     age: 14
+//   },
+//   {
+//     name: "Daniel",
+//     class: "S.S.S 1",
+//     age: 15
+//   },
+//   {
+//     name: "Ayo ola",
+//     class: "J.S.S 3",
+//     age: 20
+//   }
+// ]
+// for (let i = 0; i < container.length; i++) {
+//   let name = container[i].name;
+//   let clas = container[i].class;
+//   let age = container[i].age
+//   console.log(` This is ${name}, 
+//   He is ${age}yrs old and he is in ${clas} class
+//   `)
+// }
+const bruce = { name: "Bruce" }; const madeline = { name: "Madeline" };
+// this function isn't associated with any object, yet 
+// it's using 'this'! 
+function greet() {   return `Hello, I'm ${this.name}!`; }
+greet();                // "Hello, I'm !" - 'this' not bound 
+greet.call(bruce);      // "Hello, I'm Bruce!" - 'this' bound to 'bruce'
+ let x = greet.call(madeline);   // "Hello, I'm Madeline!" - 'this' bound to 'madeline' 
+console.log(x);
